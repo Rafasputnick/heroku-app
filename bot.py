3526f0737e86d96f167595ead2e3ef65a8a48696
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2022-06-05 23:21:51 -0300

    feat: prediction

--- bot.py
+++ bot.py
@@ -2,17 +2,14 @@ import os
 import discord
 from decouple import config
 from discord.ext import commands
-# from dotenv import load_dotenv
-
-# load_dotenv()
+import requests
+from predict import *
 
 # local
 # bot_token = os.getenv('TOKEN')
 
 # heroku
 bot_token = config('TOKEN')
-
-# bot_token = os.environ['TOKEN']
 
 # client = discord.Client()
 client = commands.Bot("$")
@@ -26,20 +23,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
       return
-    
-    # id_canal = 983118116290695238
-    # canal = client.get_channel(id_canal)
-    # await canal.send("Mensagem em canal especifico")
 
     if message.content.startswith('$teste'):
       await message.channel.send('testando 1,2,3...')
       return
-    # return
-    if message.attachments:
-        imagem = message.attachments[0].url
-        predicao = '...'
-        mensagem = 'Sua predição é: ' + predicao
-        await message.channel.send(mensagem)
 
     await client.process_commands(message)
 
@@ -48,5 +35,33 @@ async def on_message(message):
 async def send_hello(ctx):
     response = 'Olá, ' + ctx.author.name 
     await ctx.channel.send(response)
+
+@client.command(name="integrantes")
+async def send_integrantes(ctx):
+    response = 'Integrantes: \n Lucas da Silva dos Santos \n Rafael Nascimento Lourenço' 
+    await ctx.channel.send(response)
+
+@client.command(name="racas")
+async def send_racas(ctx):
+    response = ''
+    for index, raca in enumerate(racas):
+      response += f'{index + 1} - {raca}\n' 
+    await ctx.channel.send(response)
+
+@client.command(name="predicao")
+async def send_predicao(ctx):
+    if ctx.message.attachments:
+        imagem_url = ctx.message.attachments[0].url        
+        response = requests.get(imagem_url)
+        file = open("sample_image.png", "wb")
+        file.write(response.content)
+        file.close()
+        resposta_unica = predict('sample_image.png')
+        resposta = f'Predição da imagem: {resposta_unica}' 
+    else:
+      resposta = 'Por favor envie uma imagem no corpo da mensagem'
+    await ctx.channel.send(resposta)
+
+
 
 client.run(bot_token)
